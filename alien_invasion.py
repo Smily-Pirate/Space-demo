88af54ddaa424a9e8fca24480bc5acb1e2ce41c5
Ghanshyam Joshi <47424158+Ghansu@users.noreply.github.com>
2020-09-21 08:02:45 -0700

    Updated!

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,14 +15,15 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship.
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Set the background color.
     bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
         """for event in pygame.event.get():
             if event.type == pygame.QUIT:
