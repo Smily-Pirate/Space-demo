c9e3aaca48e55181beae2498533f767ed0890036
Ghanshyam Joshi <47424158+Ghansu@users.noreply.github.com>
2020-09-22 02:55:00 -0700

    Updated!

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from pygame.sprite import Group
 from ship import Ship
 import game_functions as gf
 
@@ -17,14 +18,18 @@ def run_game():
     # Make a ship.
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets in.
+    bullets = Group()
+
     # Set the background color.
     bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
         """for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()"""
