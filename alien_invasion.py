d85e5324a0a16b540152c5a2d233fa7c8ec78593
Ghanshyam Joshi <47424158+Ghansu@users.noreply.github.com>
2020-09-25 03:02:01 -0700

    Updated!

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,9 +18,9 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
-    aliens = Group()
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
+
     # Create the fleet of aliens.
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
@@ -32,8 +32,9 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-        gf.create_fleet(ai_settings, screen, ship, aliens)
+        #gf.create_fleet(ai_settings, screen, ship, aliens)
 
 
 run_game()
