00cf4aeedcdb13f06e80324630796a999aef9b4e
Ghanshyam Joshi <47424158+Ghansu@users.noreply.github.com>
2020-09-28 02:38:54 -0700

    Updated!

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from settings import Settings
 from pygame.sprite import Group
 from ship import Ship
 import game_functions as gf
+from game_stats import GameStats
 from alien import Alien
 
 
@@ -24,15 +25,20 @@ def run_game():
     # Create the fleet of aliens.
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
+    # Create an instance to store game statistics.
+    stats = GameStats(ai_settings)
+
     # Set the background color.
     bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_aliens(ai_settings, aliens)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
         #gf.create_fleet(ai_settings, screen, ship, aliens)
 
